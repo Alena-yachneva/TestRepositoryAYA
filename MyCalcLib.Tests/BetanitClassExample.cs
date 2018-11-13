869e850d1fd220c519118f1049727f7315cb7350
Alena-yachneva <45003603+Alena-yachneva@users.noreply.github.com>
2018-11-13 16:39:58 +0300

    Add files via upload

---
+++ MyCalcLib.Tests/BetanitClassExample.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace MyCalcLib.Tests
+{
+
+
+
+    [TestClass]
+
+    public class BetanitClassExample
+    {
+        [TestMethod]
+
+        public void Test2()
+        {
+            var five = 5;
+            var three = 3;
+            var eight = five + three;
+
+            Assert.AreEqual(8, eight, "must be 8");
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            //arrange
+            int z = 10;
+            int v = 20;
+            int expected = 30;
+
+            //act
+            MyCalc l = new MyCalc();
+            int actual = l.Sum(z, v);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+       /* public void Test_ForbiddenForPackageChunkWhenPackageNotFound()
+        {
+            //arrange
+            
+            var packagesRepositoryMock = _mocks.Create<IPackagesRepository>();
+            packagesRepositoryMock
+                .Setup(r => r.FindPackageAsync(_packageId))
+                .Returns(Task<DatabasePackage>.Factory.StartNew(() => null));
+            Register(packagesRepositoryMock.Object);
+
+            //act
+            var message = PostChunkToServer(new byte[] { 1, 2, 3 });
+
+            //assert
+            _mocks.VerifyAll();
+
+            Assert.That(message.StatusCode, Is.EqualTo(HttpStatusCode.Forbidden));
+        }
+        */
+    }
+}
---
+++ MyCalcLib.Tests/MyCalcTests.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Linq;
+
+
+namespace MyCalcLib.Tests
+{
+    public interface IMyInterface
+    {
+        int Sum(int a, int b);
+        
+    }
+
+
+    public interface IMyInterface2
+    {
+        int Div(int d, int e);
+
+    }
+
+    [TestClass]
+    public class ValidationTest
+    {
+
+        [TestMethod]
+        public void Two_plus_three_must_be_5()
+        {
+            var mock = new Mock<IMyInterface>();
+            mock
+                .Setup(myInterface => myInterface.Sum(It.IsAny<int>(), It.IsAny<int>()))
+                .Returns(() => 42);
+            var result = mock.Object.Sum(2, 2);
+
+            Assert.AreEqual(42, result, "it always should be 42");
+
+        }
+
+
+        [TestMethod]
+        public void Div_70_by_10_700checkInterface()
+        {
+            var mock = new Mock<IMyInterface2>();
+                mock
+                .Setup(myInterface => myInterface.Div(It.IsAny<int>(), It.IsAny<int>()))
+                .Returns(() => 700);
+            var result = mock.Object.Div(70, 10);
+
+            Assert.AreEqual(700, result);
+
+            
+
+        }
+
+
+
+       
+        [TestMethod]
+        public void Sum_10and20_30()
+        {
+            //arrange
+            int x = 10;
+            int y = 20;
+            int expected = 30;
+
+            //act
+            MyCalc c = new MyCalc();
+            int actual = c.Sum(x, y);
+
+            //assert
+            Assert.AreEqual(expected, actual);
+        }
+
+      
+        [TestMethod]
+        public void Div_50and25_2()
+        {
+            var a = 50;
+            var b = 25;
+            var c = 4;
+
+            MyCalc d = new MyCalc();
+            var result = d.Div(a, b);
+            result = result + 2;
+
+            Assert.AreEqual(c, result, "message");
+        }
+
+        [TestMethod]
+
+        public void Test_of_6multiply7_is42()
+        {
+            var u = 6;
+            var o = 7;
+            var expected = 42;
+
+            expected = 6 * 7;
+            expected = expected + 1;
+            Assert.AreEqual(43, expected, "meesage");
+        }
+
+
+        /*[TestMethod]
+        public void Data_provider_shuld_contain_my_Entity()
+        {
+            var mock = new Mock<IDataProvider>();
+            mock.Setup(DataProvider => DataProvider.EntityExists<Entity>())
+            .Returns(true);
+            mock
+                .Setup(DataProvider => DataProvider.EntityExists<OtherEntity>())
+                .Returns(false);
+            var result = Mock.Object.EntityExists<Entity>();
+
+            Assert.IsTrue(result);
+            Assert.IsFalse(Mock.Object.EntityExists<OtherEntity>());
+        
+                }
+                */
+
+    }
+
+    class Entity
+    {
+    }
+    class OtherEntity
+    {
+    }
+}
